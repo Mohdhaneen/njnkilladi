00cb1d79fd478a39d5db82c169da2d4d6b976a11
Mohd haneen <90704922+Mohdhaneen@users.noreply.github.com>
2021-12-22 19:06:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -35,7 +35,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Auto Filter V3**
+**HAI..MY NAME IS ROBERT DOWNEY JR YOU CAN USE ME IN YOUR GROUP JUST ADD ME TO YOUR GROUP AND SETT ME ADMIN THAL ALL**
 
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
