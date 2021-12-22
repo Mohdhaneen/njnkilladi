a8f8eef579230868c7bb596544ddc3700a83532d
Mohd haneen <90704922+Mohdhaneen@users.noreply.github.com>
2021-12-22 18:59:24 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+♻️ Name :  ROBERT DOWNEY JR
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+♻️ Creator : [@MOHD_HANEEN]
 
-➥ Language : Python3
+♻️ Language : Python3
 
-➥ Library : Pyrogram Asyncio 1.13.0
+♻️ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+♻️ MAIN CHANNEL : [Click Me](https://t.me/cinemacenterofficial)
 """
